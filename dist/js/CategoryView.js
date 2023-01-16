97675f7e931a2adaedf574dc4e71102b02143642
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2023-01-16 10:48:26 +0330

    [feature]: add title warning & dynamic section to categoryView

--- dist/js/CategoryView.js
+++ dist/js/CategoryView.js
@@ -1,13 +1,22 @@
 const categoryTitle = document.querySelector("#category-title");
 const categoryDescription = document.querySelector("#category-description");
 const addCategoryBtn = document.querySelector("#add-category-btn");
+const cancleCategoryBtn = document.querySelector("#cancle-category-btn");
 const categoryList = document.querySelector("#product-category");
+const categorySection = document.querySelector("#add-category");
+const categoryLink = document.querySelector("#category-link");
+const categoryTitleWarning = document.querySelector("#category-title-warning");
 class CategoryView {
     constructor() {
         addCategoryBtn === null || addCategoryBtn === void 0 ? void 0 : addCategoryBtn.addEventListener("click", (e) => {
             e.preventDefault();
             this.addCategory(e);
         });
+        cancleCategoryBtn === null || cancleCategoryBtn === void 0 ? void 0 : cancleCategoryBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            this.showCategorySection();
+        });
+        categoryLink === null || categoryLink === void 0 ? void 0 : categoryLink.addEventListener("click", () => this.showCategorySection());
         this.categories = JSON.parse(localStorage.getItem("category")) || [];
     }
     setCategory() {
@@ -18,26 +27,46 @@ class CategoryView {
         categoryList.innerHTML = list;
     }
     addCategory(e) {
-        const title = categoryTitle.value.trim().toLocaleLowerCase();
-        const description = categoryDescription.value.trim().toLocaleLowerCase();
-        const date = new Date();
-        const id = new Date().getTime();
-        const newCategory = {
-            id: id,
-            title: title,
-            description: description,
-            createdDate: date,
-        };
-        if (localStorage.getItem("category")) {
-            const categoryData = JSON.parse(localStorage.getItem("category"));
-            categoryData.push(newCategory);
-            localStorage.setItem("category", JSON.stringify(categoryData));
+        if (categoryTitle.value.trim().length > 3) {
+            const title = categoryTitle.value.trim().toLocaleLowerCase();
+            const description = categoryDescription.value.trim().toLocaleLowerCase();
+            const date = new Date();
+            const id = new Date().getTime();
+            const newCategory = {
+                id: id,
+                title: title,
+                description: description,
+                createdDate: date,
+            };
+            if (localStorage.getItem("category")) {
+                const categoryData = JSON.parse(localStorage.getItem("category"));
+                categoryData.push(newCategory);
+                localStorage.setItem("category", JSON.stringify(categoryData));
+            }
+            else {
+                localStorage.setItem("category", JSON.stringify([newCategory]));
+            }
+            this.categories = JSON.parse(localStorage.getItem("category"));
+            this.setCategory();
+            categoryTitle.classList.remove("border-red-500");
+            this.removeTitleWarning();
         }
         else {
-            localStorage.setItem("category", JSON.stringify([newCategory]));
+            categoryTitle.classList.add("border-red-500");
+            this.setTitleWarning("Category title`s characters must be longer than 3");
         }
-        this.categories = JSON.parse(localStorage.getItem("category"));
-        this.setCategory();
+    }
+    showCategorySection() {
+        categorySection.classList.toggle("hidden");
+    }
+    setTitleWarning(error) {
+        categoryTitleWarning.classList.remove("hidden");
+        categoryTitleWarning.classList.add("block");
+        categoryTitleWarning.innerText = error;
+    }
+    removeTitleWarning() {
+        categoryTitleWarning.classList.remove("block");
+        categoryTitleWarning.classList.add("hidden");
     }
 }
 export default new CategoryView();
--- src/ts/CategoryView.ts
+++ src/ts/CategoryView.ts
@@ -10,9 +10,25 @@ const addCategoryBtn = document.querySelector(
   "#add-category-btn"
 ) as HTMLInputElement;
 
+const cancleCategoryBtn = document.querySelector(
+  "#cancle-category-btn"
+) as HTMLInputElement;
+
 const categoryList = document.querySelector(
   "#product-category"
 ) as HTMLSelectElement;
+
+const categorySection = document.querySelector(
+  "#add-category"
+) as HTMLDivElement;
+
+const categoryLink = document.querySelector(
+  "#category-link"
+) as HTMLHeadElement;
+
+const categoryTitleWarning = document.querySelector(
+  "#category-title-warning"
+) as HTMLSpanElement;
 
 interface categoryType {
   id: number;
@@ -22,17 +38,22 @@ interface categoryType {
 }
 
 class CategoryView {
- categories: categoryType[];
+  categories: categoryType[];
 
   constructor() {
     addCategoryBtn?.addEventListener("click", (e) => {
       e.preventDefault();
       this.addCategory(e);
     });
+    cancleCategoryBtn?.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.showCategorySection();
+    });
+    categoryLink?.addEventListener("click", () => this.showCategorySection());
     this.categories = JSON.parse(localStorage.getItem("category")!) || [];
   }
 
-  setCategory() {
+  setCategory(): void {
     let list: string = `<option value="none" class="text bg-slate-300 dark:bg-slate-600" selected>select a category</option>`;
     this.categories.forEach((item) => {
       list += `<option class="text bg-slate-300 dark:bg-slate-600" value="${item.id}" >${item.title}</option>`;
@@ -41,27 +62,49 @@ class CategoryView {
   }
 
   addCategory(e: MouseEvent): void {
-    const title = categoryTitle.value.trim().toLocaleLowerCase();
-    const description = categoryDescription.value.trim().toLocaleLowerCase();
-    const date = new Date();
-    const id = new Date().getTime();
-    const newCategory: categoryType = {
-      id: id,
-      title: title,
-      description: description,
-      createdDate: date,
-    };
-    if (localStorage.getItem("category")) {
-      const categoryData: categoryType[] = JSON.parse(
-        localStorage.getItem("category")!
-      );
-      categoryData.push(newCategory);
-      localStorage.setItem("category", JSON.stringify(categoryData));
+    if (categoryTitle.value.trim().length > 3) {
+      const title = categoryTitle.value.trim().toLocaleLowerCase();
+      const description = categoryDescription.value.trim().toLocaleLowerCase();
+      const date = new Date();
+      const id = new Date().getTime();
+      const newCategory: categoryType = {
+        id: id,
+        title: title,
+        description: description,
+        createdDate: date,
+      };
+      if (localStorage.getItem("category")) {
+        const categoryData: categoryType[] = JSON.parse(
+          localStorage.getItem("category")!
+        );
+        categoryData.push(newCategory);
+        localStorage.setItem("category", JSON.stringify(categoryData));
+      } else {
+        localStorage.setItem("category", JSON.stringify([newCategory]));
+      }
+      this.categories = JSON.parse(localStorage.getItem("category")!);
+      this.setCategory();
+      categoryTitle.classList.remove("border-red-500");
+      this.removeTitleWarning();
     } else {
-      localStorage.setItem("category", JSON.stringify([newCategory]));
+      categoryTitle.classList.add("border-red-500");
+      this.setTitleWarning("Category title`s characters must be longer than 3");
     }
-    this.categories = JSON.parse(localStorage.getItem("category")!);
-    this.setCategory();
+  }
+
+  showCategorySection(): void {
+    categorySection.classList.toggle("hidden");
+  }
+
+  setTitleWarning(error: string):void {
+    categoryTitleWarning.classList.remove("hidden");
+    categoryTitleWarning.classList.add("block");
+    categoryTitleWarning.innerText = error;
+  }
+
+  removeTitleWarning():void{
+    categoryTitleWarning.classList.remove("block");
+    categoryTitleWarning.classList.add("hidden");
   }
 }
 
